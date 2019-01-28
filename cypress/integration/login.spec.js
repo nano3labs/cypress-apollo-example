929109040299a22bf7b9379d7df13757d29f55e0
Michael Yagudaev <michaelyagudaev@gmail.com>
2019-01-28 13:25:56 -0800

    add logout test

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,8 +1,16 @@
 describe('login', () => {
-  it('should be able to login with any email', () => {
+  before(() => {
     cy.visit('http://localhost:3000')
     cy.get('[name="email"]').type('michael@example.com')
     cy.contains('Log in').click()
-    expect(cy.contains('Space Explorer'))
+  })
+
+  it('should be able to login with any email', () => {
+    expect(cy.contains('Logout'))
+  })
+
+  it ('should let me logout', () => {
+    cy.contains('Logout').click()
+    expect(cy.contains('Log in'))
   })
 })
