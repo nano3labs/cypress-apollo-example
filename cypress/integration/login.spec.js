67f925821a5a15bf2aea18c6452fa5b2baa7c32d
Michael Yagudaev <michaelyagudaev@gmail.com>
2019-01-28 13:17:20 -0800

    login test

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,5 +1,8 @@
 describe('login', () => {
   it('should be able to login with any email', () => {
     cy.visit('http://localhost:3000')
+    cy.get('[name="email"]').type('michael@example.com')
+    cy.contains('Log in').click()
+    expect(cy.contains('Space Explorer'))
   })
 })
